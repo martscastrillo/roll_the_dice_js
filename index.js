435713134b59f9eac4bbd63537c513b5c88ed105
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-18 12:24:50 +0200

    add roll the dice

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 "use strict";
 
-let elDiceOne = document.getElementById("dice1");
-let elDiceTwo = document.getElementById("dice2");
-let elComeOut = document.getElementById("roll");
+let elDiceOne = document.querySelector(".dice-one");
+let elDiceTwo = document.querySelector(".dice-two");
+let elComeOut = document.querySelector(".roll-button");
+
+elComeOut.onClick = function () { rollDice();};
+
+function rollDice() {
+    let diceOne = Math.floor((Math.random() * 6) + 1);
+    let diceTwo = Math.floor((Math.random() * 6) + 1);
+      console.log(diceOne + " " + diceTwo);
+      for (let index = 1; index <= 6; index++) {
+          elDiceOne.classList.remove('show-' + index);
+          if(diceOne === index){
+              elDiceOne.classList.add('show-' + index);
+          }
+      }
+      for (let index = 1; index <= 6; index++) {
+          elDiceTwo.classList.remove('show-' + index);
+          if(diceTwo === index){
+              elDiceTwo.classList.add('show-' + index);
+          }
+      }
+      setTimeout(rollDice(), 1000);
+}
 
 
+  
 
-elComeOut.addEventListener("click", ()=>{
-  let diceOne = Math.floor((Math.random() * 6) + 1);
-  let diceTwo = Math.floor((Math.random() * 6) + 1);
-    console.log(diceOne + "" + diceTwo);
-    for (let index = 0; index <= index; index++) {
-        elDiceOne.classList.remove('show-' + index);
-        if(diceOne === index){
-            elDiceOne.classList.add('show-' + index);
-        }
-    }
-    for (let index = 0; index <= index; index++) {
-        elDiceTwo.classList.remove('show-' + index);
-        if(diceTwo === index){
-            elDiceTwo.classList.add('show-' + index);
-        }
-    }
-    setTimeout(rollDice(), 1000);
-});
