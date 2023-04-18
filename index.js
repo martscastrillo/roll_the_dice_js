e5a27d4c6b1a65760427f997b62df78ae8632515
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-18 12:51:13 +0200

    add final result

--- index.js
+++ index.js
@@ -4,26 +4,31 @@ let elDiceOne = document.querySelector(".dice-one");
 let elDiceTwo = document.querySelector(".dice-two");
 let elComeOut = document.querySelector(".roll-button");
 
-elComeOut.onClick = function () { rollDice();};
-
-function rollDice() {
+function handleClick() {
     let diceOne = Math.floor((Math.random() * 6) + 1);
     let diceTwo = Math.floor((Math.random() * 6) + 1);
-      console.log(diceOne + " " + diceTwo);
-      for (let index = 1; index <= 6; index++) {
+     for (let index = 1; index <= 6; index++) {
           elDiceOne.classList.remove('show-' + index);
           if(diceOne === index){
               elDiceOne.classList.add('show-' + index);
           }
+      
       }
       for (let index = 1; index <= 6; index++) {
           elDiceTwo.classList.remove('show-' + index);
           if(diceTwo === index){
               elDiceTwo.classList.add('show-' + index);
           }
+     
       }
-      setTimeout(rollDice(), 1000);
+/*        setTimeout(handleClick(), 1000); */
 }
+
+elComeOut.addEventListener("click", handleClick);
+
+
+
+
 
 
   
